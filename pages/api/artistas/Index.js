7c0fd285c78b7f211ad24b19d5680a4090d77b86
Fredson Junior <fredsonjunior5209@gmail.com>
2024-07-17 17:19:09 -0500

    Adiciona lógica da API de artistas

--- pages/api/artistas/Index.js
+++ pages/api/artistas/Index.js
@@ -1,21 +1,28 @@
-import syncDatabase from '../../../models/Index'; // Importa a função syncDatabase do arquivo index.js dentro de models
-
-import Artista from '../../../models/Artista'; // Importa o modelo Artista
-// import Album from '../../../models/Album'; // Importa o modelo Album
-// import Musica from '../../../models/Musica'; // Importa o modelo Musica
+import sequelize from '../../lib/sequelize';
+import Artista from '../../models/Artista';
 
 export default async function handler(req, res) {
-  await syncDatabase(); // Sincroniza o banco de dados
+  await sequelize.sync({ force: true }); // Sincroniza o banco de dados
 
   if (req.method === 'GET') {
-    const artistas = await Artista.findAll();
-    res.status(200).json(artistas);
+    try {
+      const artistas = await Artista.findAll();
+      res.status(200).json(artistas);
+    } catch (error) {
+      console.error('Erro ao buscar artistas:', error);
+      res.status(500).json({ error: 'Erro ao buscar artistas.' });
+    }
   } else if (req.method === 'POST') {
-    const { nome, genero, nacionalidade, data_nascimento } = req.body;
-    const novoArtista = await Artista.create({ nome, genero, nacionalidade, data_nascimento });
-    res.status(201).json(novoArtista);
+    try {
+      const { nome, genero, nacionalidade, data_nascimento } = req.body;
+      const novoArtista = await Artista.create({ nome, genero, nacionalidade, data_nascimento });
+      res.status(201).json(novoArtista);
+    } catch (error) {
+      console.error('Erro ao criar artista:', error);
+      res.status(500).json({ error: 'Erro ao criar artista.' });
+    }
   } else {
     res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.status(405).end(`Método ${req.method} não permitido`);
   }
 }
